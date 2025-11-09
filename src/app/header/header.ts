a26d0ac40be50c9ec9ed6b576a9dc9775996523d
markmolloy <markmolloy57@gmail.com>
2025-11-09 10:43:42 -0500

    Changed header style. Added download button

--- src/app/header/header.ts
+++ src/app/header/header.ts
@@ -1,16 +1,21 @@
 import { Component, Output, EventEmitter } from '@angular/core';
+import { LucideAngularModule, Camera, Download } from 'lucide-angular';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [LucideAngularModule],
   templateUrl: './header.html',
   styleUrl: './header.scss',
 })
 export class Header {
+  readonly Camera = Camera;
+  readonly Download = Download;
   
   @Output() screenshot = new EventEmitter<void>();
 
   toggleScreenshot() {
     this.screenshot.emit();
   }
+
+  saveModal() {}
 }
